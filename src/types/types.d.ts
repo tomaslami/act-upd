d6d8e19e94667b1d19234bb45d1d01edeb9f5daf
tomaslami <tomaslamiguralnik@gmail.com>
2025-02-03 15:28:36 -0300

    types error fixed

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,22 +1,33 @@
-
-export interface Cursos {
-  value: string,
-  bgcolor: string,
-  text: string,
-  title: string,
-  subtitle: string,
-  date: string,
-  modality?: string,
-  timemorning: string,
-  timeafternoon: string,
-  about: string
-  topics: string[],
-  objectives?: string[],
-  cv?: string,
-  button: string,
-}
+export type Cursos = {
+  value: string;
+  title: string;
+  text: string;
+  bgcolor: string;
+  subtitle: string;
+  date: string;
+  modality: string;
+  timemorning: string;
+  timeafternoon: string;
+  about: string;
+  objectives?: string[];
+  topics: string[];
+  includes?: string;
+  instructor?: string | string[];
+  button: string;
+  contact?: {
+    email: string;
+    whatsapp: string;
+  };
+  recommendations?: string;
+  coordination?: string;
+  registration_note?: string;
+  price?: string;
+  payment_methods?: string[];
+  attendance?: string;
+  cv?: string;
+};
 
 export interface ServerActionResponse {
-  status: 200 | 500
-  message: string | string[]
+  status: 200 | 500;
+  message: string | string[];
 }
